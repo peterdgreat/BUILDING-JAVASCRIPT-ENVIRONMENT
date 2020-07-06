ed8454fa3f8d73b26b3ec8b3cb54d49352e4f752
Ajayi Peter <peterdgreat2016@gmail.com>
2020-07-06 21:18:20 +0100

    changed

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from  'path';
+import open from 'open';
+
+const port = 3001;
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '../src/index.html'));
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
